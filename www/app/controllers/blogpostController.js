8da80d22bb23d5013f914b2a8685704a2bf6b531
LgiMatBrd <contato@skorp.com.br>
2017-09-13 18:14:48 -0300

    antes de modificar tudo dnv

--- www/app/controllers/blogpostController.js
+++ www/app/controllers/blogpostController.js
@@ -1,20 +1,9 @@
-angular.module('microAzureBlog', [])
-	.controller('blogpostController', function ($scope, $http) {
-		
-		$scope.model = {
-			// posts: [
-			// 	{
-			// 	title: "Sample Title",
-			// 	text: "Sample Text"
-			// 	},
-			// 	{
-			// 	title: "Sample Title 2",
-			// 	text: "Sample Text 2"
-			// 	}
-			// ]
-		};
+angular.module('microAzureBlog', ['azure-mobile-service'])
+angular.module('your-module-name').constant('AzureMobileServiceClient', {
+    API_URL: 'https://<your-api-url>.azure-mobile.net/',
+    API_KEY: '<your-api-key>',
+})
+.controller('blogpostController', function ($scope, $http, Azureservice) {
 
-        $http.get('https://doutorsofa.azurewebsites.net/lista/Todoitem').success(function(data) {
-			$scope.model.posts = data;
-		});
-	});
+});
+
