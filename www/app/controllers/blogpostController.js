0db452016e061f86f9a504c8abb74e2cdf427a4d
LgiMatBrd <contato@skorp.com.br>
2017-09-13 18:15:12 -0300

    Mudando tudo de novo

--- www/app/controllers/blogpostController.js
+++
@@ -1,9 +0,0 @@
-angular.module('microAzureBlog', ['azure-mobile-service'])
-angular.module('your-module-name').constant('AzureMobileServiceClient', {
-    API_URL: 'https://<your-api-url>.azure-mobile.net/',
-    API_KEY: '<your-api-key>',
-})
-.controller('blogpostController', function ($scope, $http, Azureservice) {
-
-});
-
