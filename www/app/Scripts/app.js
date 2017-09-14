93c38022497f63676938bb1e4bbadb90022669b3
LgiMatBrd <contato@skorp.com.br>
2017-09-14 11:35:57 -0300

    master

--- www/app/Scripts/app.js
+++ www/app/Scripts/app.js
@@ -1,7 +1,7 @@
 ﻿'use strict';
 angular.module('todoApp', ['ngRoute', 'azure-mobile-service.module']).constant('AzureMobileServiceClient', {
-    API_URL : 'yourmobileservice',
-    API_KEY : 'appkey',
+    API_URL: 'https://VisualStudio/SPNc473c603-e199-4d3c-be06-a918d8134f54',
+    API_KEY: '0697b073-ee12-4c7b-9077-5f65b9eafbe3',
   })
 .config(['$routeProvider', '$httpProvider', function ($routeProvider, $httpProvider) {
     
