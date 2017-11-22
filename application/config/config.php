5e83758e30e70cd93416fd4a41fc25256a883bee
LgiMatBrd <contato@skorp.com.br>
2017-11-22 15:12:09 -0200

    config.php

--- application/config/config.php
+++ application/config/config.php
@@ -377,10 +377,10 @@ $config['encryption_key'] = 'lrs3hpbr';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';//enable session with db
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';//database table name
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
